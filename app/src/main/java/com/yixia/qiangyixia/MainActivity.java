537b46b7073de878104efc4d7db29ff253e37d4e
duanyikang <574281045@qq.com>
2017-01-23 09:25:58 +0800

    首次提交

---
+++ app/src/main/java/com/yixia/qiangyixia/MainActivity.java
@@ -0,0 +1,23 @@
+package com.yixia.qiangyixia;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(android.provider.Settings.ACTION_ACCESSIBILITY_SETTINGS);
+        startActivity(intent);
+    }
+
+
+}
---
+++ app/src/main/java/com/yixia/qiangyixia/util/Constant.java
@@ -0,0 +1,12 @@
+package com.yixia.qiangyixia.util;
+
+/**
+ * 作者： duanyikang on 2017/1/20.
+ * 邮箱： duanyikang@yixia.com
+ * 描述：
+ */
+
+public interface Constant {
+    String IS_RUNNING_SERVICE="IS_RUNNING_SERVICE";//自助服务是否开启
+    String WECHAT_UI_LIST_COUNT = "wechat_ui_list_count";//聊天界面消息个数
+}
